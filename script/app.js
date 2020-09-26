73dc0678f92e5b7b290d0cb49e08a780c699fa3a
Abe <abhi.rawat456@gmail.com>
2020-09-26 13:04:22 +0530

    Added navbar

--- script/app.js
+++ script/app.js
@@ -1,7 +1,12 @@
 var prev = document.getElementById("prev");
 var next = document.getElementById("next");
 var hero = document.getElementById("hero");
+var ham = document.getElementById("ham");
+var mobile = document.getElementById("mobile");
+var logo = document.getElementById("logo");
+var list = document.getElementById("list");
 var current = 1;
+var cross = false;
 var firstImage = "url(../images/desktop-image-hero-1.jpg)";
 var secondImage = "url(../images/desktop-image-hero-2.jpg)";
 var thirdImage = "url(../images/desktop-image-hero-3.jpg)";
@@ -31,5 +36,21 @@ next.addEventListener("click", function () {
     else if (current == 3) {
         hero.style.backgroundImage = firstImage;
         current = 1;
+    }
+});
+ham.addEventListener("click", function () {
+    if (!cross) {
+        ham.src = "./images/icon-close.svg";
+        logo.style.display = "none";
+        mobile.style.backgroundColor = "white";
+        list.style.display = "flex";
+        cross = true;
+    }
+    else {
+        ham.src = "./images/icon-hamburger.svg";
+        mobile.style.backgroundColor = "transparent";
+        logo.style.display = "block";
+        list.style.display = "none";
+        cross = false;
     }
 });
--- script/app.ts
+++ script/app.ts
@@ -1,8 +1,13 @@
 const prev = document.getElementById("prev") as HTMLDivElement;
 const next = document.getElementById("next") as HTMLDivElement;
 const hero = document.getElementById("hero") as HTMLDivElement;
+const ham = document.getElementById("ham") as HTMLImageElement;
+const mobile = document.getElementById("mobile") as HTMLDivElement;
+const logo = document.getElementById("logo") as HTMLParagraphElement;
+const list = document.getElementById("list") as HTMLUListElement;
 
 let current = 1;
+let cross = false;
 let firstImage = "url(../images/desktop-image-hero-1.jpg)";
 let secondImage = "url(../images/desktop-image-hero-2.jpg)";
 let thirdImage = "url(../images/desktop-image-hero-3.jpg)";
@@ -30,5 +35,21 @@ next.addEventListener("click", () => {
   } else if (current == 3) {
     hero.style.backgroundImage = firstImage;
     current = 1;
+  }
+});
+
+ham.addEventListener("click", () => {
+  if (!cross) {
+    ham.src = "./images/icon-close.svg";
+    logo.style.display = "none";
+    mobile.style.backgroundColor = "white";
+    list.style.display = "flex";
+    cross = true;
+  } else {
+    ham.src = "./images/icon-hamburger.svg";
+    mobile.style.backgroundColor = "transparent";
+    logo.style.display = "block";
+    list.style.display = "none";
+    cross = false;
   }
 });
